c6b7701cc942ca1dc5b7561aebdd42d1645d8800
EC2 Default User <ec2-user@ip-172-31-31-10.ap-northeast-1.compute.internal>
2024-07-13 05:16:40 +0000

    [デプロイ中2]

--- db/migrate/20240708120526_devise_create_customers.rb
+++ db/migrate/20240708120526_devise_create_customers.rb
@@ -32,7 +32,7 @@ class DeviseCreateCustomers < ActiveRecord::Migration[6.1]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
       t.string :name, null: false, default: ""
-      t.text :introduction, null: false, default: ""
+      t.text :introduction, null: false, default: -> {("")}
       t.boolean :is_active, null: false, default: true
 
       t.timestamps null: false
