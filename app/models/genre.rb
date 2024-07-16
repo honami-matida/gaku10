765d829dd7f43560232bb9eadc72cdbef65c2a3a
EC2 Default User <ec2-user@ip-172-31-31-10.ap-northeast-1.compute.internal>
2024-07-16 12:02:30 +0000

    1stデプロイ+テスト完了

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,5 +2,7 @@ class Genre < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :groups, dependent: :destroy
 
+  validates :name, presence: true
+
   #validates :name, uniqueness: true, presence: true
 end
