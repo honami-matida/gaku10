82104fbffb5959c6ea030a35fb9d308791e986e9
EC2 Default User <50reef05@gmail.com>
2024-08-18 06:07:49 +0000

    いいね・コメントの非同期通信化

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -2,11 +2,12 @@ class Public::PostCommentsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
+    @post_comments = @post.post_comments.all.page(params[:page]).per(9).order(created_at: :desc)
     @post_comment = PostComment.new(post_comment_params)
     @post_comment.customer_id = current_customer.id
     @post_comment.post_id = @post.id
     if @post_comment.save
-      redirect_to public_post_path(@post)
+
     else
       @post_comments = @post.post_comments.all.page(params[:page]).per(9).order(created_at: :desc)
       flash.now[:notice] = "送信に失敗しました"
@@ -16,10 +17,11 @@ class Public::PostCommentsController < ApplicationController
 
   def destroy
     @post = Post.find(params[:post_id])
+    @post_comments = @post.post_comments.all.page(params[:page]).per(9).order(created_at: :desc)
     post_comment = PostComment.find(params[:id])
     post_comment.destroy
     flash[:notice] = "コメントを削除しました"
-    redirect_to public_post_path(@post)
+
   end
 
   private
